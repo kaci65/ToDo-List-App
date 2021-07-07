fea7d56a68f44a617b3c6df1465a4d983ebbcd6f
eowyn <eowyn@localhost.localdomain>
2021-07-07 23:52:19 +0300

    Update => DB

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+const mongoose = require('mongoose');
+//connection to db
+mongoose.set("useFindAndModify", false);
+mongoose.connect(
+    process.env.MONGO_URI,
+    {useNewUrlParser: true}, () => {
+        console.log("DB connected ;)");
+        app.listen(3000, () =>
+            console.log("Server up and running! :)"));
+    });
 
 app.use("/static", express.static("public"));
 app.use(express.urlencoded({extended: true}));
@@ -16,8 +29,3 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
     console.log(req.body);
 });
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server up and learning. Node.js api listening from ${port}`);    
-}); 
